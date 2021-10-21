990918bb4f0166a75e3b31730a7c137e182992da
467708971 <467708971@qq.com>
2021-10-21 11:17:59 +0800

    按照题目要求更改了内容

--- part02/index08.js
+++ part02/index08.js
@@ -1,12 +1,5 @@
-const test = dom.find('#test03')[0];
-console.log(test);
+const div = dom.find('#test01>.test11')[0]
+dom.style(div,'color','red')
 
-dom.style(test01,{outline:'1px solid red',color:'green',backgroundColor:'yellow'})
-dom.style(test02,'background-color','pink')
-
-
-dom.each(test01.children,(a)=>{
-    dom.style(
-        a,{outline:'1px solid green',color:'purple'}
-    )
-})
+const divList = dom.find('#test01') 
+dom.each(divList, (n)=> console.log(n))
